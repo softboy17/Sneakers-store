6f28ff47b9dadd6cc4812123fe521f5bbc5499a0
softboy17 <kojcubekovryskeldi@gmail.com>
2023-03-05 03:22:32 +0600

    I made header && footer

---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='w-[80%] py-[5%] mx-auto my-72 bg-gray-500 text-white'>
+            <h2 className='text-center bg-gray-500 text-white'>Copyright © 2022 Sneakers-store, Inc. All Rights Reserved.</h2>
+        </div>
+    );
+};
+
+export default Footer;
